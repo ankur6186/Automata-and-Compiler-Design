d49afb6790495517b68750535bc1fbb7f50abaa1
Ankur Singh <39549833+ankur6925@users.noreply.github.com>
2019-08-22 05:05:52 +0530

    Add files via upload

---
+++ lab1/p2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int i=0;
+	scanf("%d",&i);
+	scanf("%d",&i);
+	scanf("%d",&i);
+	printf("%id"i);
+
+	return 0;
+}
---
+++ lab1/p4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	long int i;
+	// ankur is king
+	double a;
+	float k;
+	// gaurav is maadarchod
+	char c;
+
+	return 0;
+}
